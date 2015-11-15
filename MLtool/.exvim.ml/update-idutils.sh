f332574252d1224eec58af1aa636b565c515ff4d
septicmk <septic.mk@gmail.com>
2015-11-15 22:28:19 +0800

    del exvim

--- MLtool/.exvim.ml/update-idutils.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export DEST="./.exvim.ml"
-export TOOLS="/home/ylf/.vim/tools/"
-export TMP="${DEST}/_ID"
-export TARGET="${DEST}/ID"
-sh ${TOOLS}/shell/bash/update-idutils.sh
--- MLtool/.exvim.ml/update-symbols.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export DEST="./.exvim.ml"
-export TOOLS="/home/ylf/.vim/tools/"
-export TMP="${DEST}/_symbols"
-export TARGET="${DEST}/symbols"
-sh ${TOOLS}/shell/bash/update-symbols.sh
