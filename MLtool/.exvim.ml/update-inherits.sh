09c3bd6621c9d57f10dc22230f663a8ad4db51bf
ylf <ylf@ga86.(none)>
2015-11-27 13:16:04 +0800

    Flexible_vector

--- MLtool/.exvim.ml/update-inherits.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export DEST="./.exvim.ml"
-export TOOLS="/home/ylf/.vim/tools/"
-export TMP="${DEST}/_inherits"
-export TARGET="${DEST}/inherits"
-sh ${TOOLS}/shell/bash/update-inherits.sh
--- MLtool/.exvim.ml/update-tags.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export DEST="./.exvim.ml"
-export TOOLS="/home/ylf/.vim/tools/"
-export CTAGS_CMD="ctags"
-export OPTIONS="--fields=+iaS --extra=+q"
-export TMP="${DEST}/_tags"
-export TARGET="${DEST}/tags"
-sh ${TOOLS}/shell/bash/update-tags.sh
